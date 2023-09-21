74ce3712feff94f36aae8633c9d9de446b683f90
Reuben Frankel <rfrankel@matatika.com>
2023-09-21 11:07:53 +0100

    Remove bad `is_token_valid` override and unnecessary constructor property declarations for `ProxyGoogleAdsAuthenticator`

--- tap_googleads/auth.py
+++ tap_googleads/auth.py
@@ -2,10 +2,9 @@
 
 
 import json
-from datetime import datetime
 from typing import Optional
-import requests
 
+import requests
 from singer_sdk.authenticators import OAuthAuthenticator, SingletonMeta
 from singer_sdk.helpers._util import utc_now
 from singer_sdk.streams import Stream as RESTStreamBase
@@ -29,31 +28,14 @@ class ProxyGoogleAdsAuthenticator(OAuthAuthenticator, metaclass=SingletonMeta):
             auth_endpoint: API username.
             oauth_scopes: API password.
         """
-        super().__init__(stream=stream)
-        self._auth_endpoint = auth_endpoint
-        self._oauth_scopes = oauth_scopes
+        super().__init__(
+            stream=stream,
+            auth_endpoint=auth_endpoint,
+            oauth_scopes=oauth_scopes,
+        )
+
         self._auth_headers = auth_headers
         self._auth_body = auth_body
-
-        # Initialize internal tracking attributes
-        self.access_token: Optional[str] = None
-        self.refresh_token: Optional[str] = None
-        self.last_refreshed: Optional[datetime] = None
-        self.expires_in: Optional[int] = None
-
-    def is_token_valid(self) -> bool:
-        """Check if token is valid.
-
-        Returns:
-            True if the token is valid (fresh).
-        """
-        if self.last_refreshed is None:
-            return False
-        if not self.expires_in:
-            return True
-        if self.expires_in > (datetime.now() - self.last_refreshed).total_seconds():
-            return True
-        return False
 
     # Authentication and refresh
     def update_access_token(self) -> None:
