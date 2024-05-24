39b8cd705f011a673245f10c893971da938b4b72
ReubenFrankel <60552974+ReubenFrankel@users.noreply.github.com>
2024-05-24 09:18:04 +0100

    Bump to `v16` API endpoints (#17)

--- tap_googleads/client.py
+++ tap_googleads/client.py
@@ -16,7 +16,7 @@ from tap_googleads.auth import GoogleAdsAuthenticator, ProxyGoogleAdsAuthenticat
 class GoogleAdsStream(RESTStream):
     """GoogleAds stream class."""
 
-    url_base = "https://googleads.googleapis.com/v14"
+    url_base = "https://googleads.googleapis.com/v16"
 
     records_jsonpath = "$[*]"  # Or override `parse_response`.
     next_page_token_jsonpath = "$.nextPageToken"  # Or override `get_next_page_token`.
