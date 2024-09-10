765723619956c8f77b1eb52483aabe97a319608b
Tobias Cadee <tobias.cadee@ticketswap.com>
2024-09-10 14:32:07 +0200

    feat(): fix the parent-child logic
    At the moment `AccessibleCustomers` is not being used as a parent stream to `CustomerHierarchyStream`. This update fixes that. By updating to the latest singer-sdk version we can use `generate_child_contexts` to generate multiple child streams per parent stream to sync all accessible customers.
    
    I also added a check to make sure a customer is enabled to avoid 403 errors.
    
    Also changed some of the tap config because in my opinion it was a bit unclear. `login_customer_id` is now required as it is required in the header. You can also specify `customer_ids` to filter on customers you want to sync. But only if you have access to them and they are enabled. I understand that some people need to update their config but in the end I think this is more clear.
    
    Moved pagination to the new singer-sdk pagination setup: https://sdk.meltano.com/en/v0.40.0/guides/pagination-classes.html
    
    Other updates are linting updates.

--- tap_googleads/tap.py
+++ tap_googleads/tap.py
@@ -45,43 +45,43 @@ class TapGoogleAds(Tap):
         th.Property(
             "oauth_credentials.client_id",
             th.StringType,
+            required=True,
+            secret=True,
         ),
         th.Property(
             "oauth_credentials.client_secret",
             th.StringType,
+            required=True,
+            secret=True,
         ),
-        th.Property(
-            "developer_token",
-            th.StringType,
-        ),
+        th.Property("developer_token", th.StringType, required=True, secret=True),
         th.Property(
             "oauth_credentials.refresh_token",
             th.StringType,
-        ),
-        th.Property(
-            "customer_id",
-            th.StringType,
+            required=True,
+            secret=True,
         ),
         th.Property(
             "login_customer_id",
             th.StringType,
-            description="Value to use in the login-customer-id header, if different from the customer_id to sync. Useful if you are syncing using a manager account.",
+            description="Value to use in the login-customer-id header. See https://developers.google.com/search-ads/reporting/concepts/login-customer-id for more info.",
+            required=True,
         ),
         th.Property(
-            "comma_separated_string_of_customer_ids",
-            th.StringType,
-            description="Overrides the taps default get all data for all available customers logic, and will get you the data for only the the provided customer_ids",
+            "customer_ids",
+            th.ArrayType(th.StringType),
+            description="Overrides the taps default get all data for all available customers logic, and will get you the data for only the the provided customer_ids, granted you have access to them.",
         ),
         th.Property(
             "enable_click_view_report_stream",
             th.BooleanType,
             description="Enables the tap's ClickViewReportStream. This requires setting up / permission on your google ads account(s)",
+            default=False,
         ),
     ).to_dict()
 
     def discover_streams(self) -> List[Stream]:
         """Return a list of discovered streams."""
-        if self.config.get("enable_click_view_report_stream"):
-            if self.config.get("enable_click_view_report_stream") == True:
-                STREAM_TYPES.append(ClickViewReportStream)
+        if self.config["enable_click_view_report_stream"]:
+            STREAM_TYPES.append(ClickViewReportStream)
         return [stream_class(tap=self) for stream_class in STREAM_TYPES]
